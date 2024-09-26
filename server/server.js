5b4c4902f13a1802a8d0d78d7f1672651dfc7174
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-09-26 17:02:15 +0300

    add

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
 
+const app = express()
 
 
-
+app.listen(process.env.PORT, () => {
+    console.log("The server is running on port 3000")
+})
