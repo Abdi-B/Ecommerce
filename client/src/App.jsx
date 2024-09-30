6e03da406fa6a339bc5134f29364c47d9f886a1a
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-09-30 15:57:23 +0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,16 @@ import { Route, Routes } from "react-router-dom";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
 import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminProducts from "./pages/admin-view/products";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found/index";
+import ShoppingAccount from './pages/shopping-view/account';
+import ShoppingListing from './pages/shopping-view/listing';
+import ShoppingHome from './pages/shopping-view/home';
+import ShoppingCheckout from './pages/shopping-view/checkout';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,17 +25,26 @@ function App() {
     <>
       {/* <h2>Ecommerce</h2> */}
       <div className="flex flex-col overflow-hidden bg-white">
-        <h1>Header Component</h1>
+        {/* <h1>Header Component</h1> */}
 
         <Routes>
           <Route path="/auth" element={<AuthLayout />}>
             <Route path="login" element={<AuthLogin />}></Route>
             <Route path="register" element={<AuthRegister />}></Route>
           </Route>
-          <Route path="/admin" element={<AdminLayout/>} >
-          <Route path="login" element={<AuthLogin />}></Route>
-
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="dashboard" element={<AdminDashboard />}></Route>
+            <Route path="products" element={<AdminProducts />}></Route>
+            <Route path="features" element={<AdminFeatures />}></Route>
+            <Route path="orders" element={<AdminOrders />}></Route>
           </Route>
+          <Route path="/shop" element={<ShoppingLayout />}>
+            <Route path="home" element={<ShoppingHome />} />
+            <Route path="listing" element={<ShoppingListing />} />
+            <Route path="account" element={<ShoppingAccount />} />
+            <Route path="checkout" element={<ShoppingCheckout/>} />
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
     </>
