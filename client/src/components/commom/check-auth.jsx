a2d47e5401b7584bdf14fee0fed3dc93c9c48462
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-10-01 13:31:57 +0300

    update

--- client/src/components/commom/check-auth.jsx
+++ client/src/components/commom/check-auth.jsx
@@ -1,19 +1,37 @@
+function CheckAut({ isAuthenticated, user, children }) {
+  const location = useLocation();
 
-function CheckAut({isAuthenticated, user, children}) {
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
 
-    const location = useLocation();
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("register"))
+  ) {
 
-    if(
-        !isAuthenticated &&
-        !(
-            location.pathname.includes('/login') ||
-            location.pathname.includes('/register')
-        )
-    ) {
-        return <Navigate to='/auth/login' />
-      }
+    if(user?.role === 'admin') {
+        return <Navigate to='/admin/dashboard' />
+    } else {
+        <Navigate to="/shop/home" />
     }
 
-    
+    if(isAuthenticated && user?.role !== 'admin' && location.pathname.includes('/admin')) {
+        return <Navigate to="/unauth-page" />
+    }
+    if (isAuthenticated && user?.role == 'admin' && location.pathname.includes('/shop')) {
+        return <Navigate to="admin/dashboard" />
+    }
+  }
+
+  return <>{ children }</>
+}
 
 export default CheckAut;
